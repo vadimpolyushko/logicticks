9fe9f5656509c2b03cd273fdc9587de9b082d77b
vadimpolyushko <93038446+vadimpolyushko@users.noreply.github.com>
2021-12-19 02:34:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.auto__scroll-img').slick({
+      prevArrow: '<button class="scrol__next" > <img src="./images/icons/Arrowright.svg" alt=""></button>',
+      nextArrow:'<button class="scrol__back" ><img src="./images/icons/Arrowleft.svg" alt=""></button>',
+    });
+  });
